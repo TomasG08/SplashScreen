23f8cac97d833f1d66c300f3aaf2d249389a428b
TomasG08 <tguzman_08@hotmail.com>
2021-02-04 14:37:34 -0600

    Se agregaron algunos detalles de diseño

--- app/src/main/java/com/cursoandroid/splashscreen/MainActivity.java
+++ app/src/main/java/com/cursoandroid/splashscreen/MainActivity.java
@@ -2,10 +2,14 @@ package com.cursoandroid.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -42,8 +46,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(logoImageView, "logoImageTrans");
+                pairs[1] = new Pair<View, String>(tomasnecrisTextView, "textTrans");
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                }else{
+                    startActivity(intent);
+                    finish();
+                }
             }
         },4000);
 
