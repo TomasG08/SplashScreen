1f4b2aca49e03ada7599a61cc060257aebdc8b7d
TomasG08 <tguzman_08@hotmail.com>
2021-02-04 18:59:34 -0600

    Se agregó SignUp y algunas animaciones.

--- app/src/main/java/com/cursoandroid/splashscreen/LoginActivity.java
+++ app/src/main/java/com/cursoandroid/splashscreen/LoginActivity.java
@@ -2,14 +2,58 @@ package com.cursoandroid.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView bienvenidoLabel, continuarLabel, nuevoUsuario;
+    ImageView loginImageView;
+    TextInputLayout usuarioTextField, contrasenaTextField;
+    MaterialButton inicioSesion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTitle("Login");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginImageView = findViewById(R.id.loginImageView);
+        bienvenidoLabel = findViewById(R.id.bienvenidoLabel);
+        continuarLabel = findViewById(R.id.continuarLabel);
+        usuarioTextField = findViewById(R.id.usuarioTextField);
+        contrasenaTextField = findViewById(R.id.contrasenaTextField);
+        inicioSesion = findViewById(R.id.inicioSesionBtn);
+        nuevoUsuario = findViewById(R.id.nuevoUsuario);
+
+        nuevoUsuario.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+
+            Pair[] pairs = new Pair[7];
+            pairs[0] = new Pair<View, String>(loginImageView, "logoImageTrans");
+            pairs[1] = new Pair<View, String>(bienvenidoLabel, "textTrans");
+            pairs[2] = new Pair<View, String>(continuarLabel, "iniciaSesionTextTrans");
+            pairs[3] = new Pair<View, String>(usuarioTextField, "emailInputTextTrans");
+            pairs[4] = new Pair<View, String>(contrasenaTextField, "passwordInputTextTrans");
+            pairs[5] = new Pair<View, String>(inicioSesion, "buttonSignInTrans");
+            pairs[6] = new Pair<View, String>(nuevoUsuario, "newUserTrans");
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                 ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
+                startActivity(intent, options.toBundle());
+            }else{
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
